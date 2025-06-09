34ed6c8e95f306c31c1bf040d83b87d9ef908cf1
Rudrapotey910 <rudrapotey910@gmail.com>
2025-06-09 23:25:55 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,18 @@
+#Assingment 1
+#Task1
+number_1=int(input('enter the number:'))
+number_2=int(input('enter the number'))
+addition=(number_1+number_2)
+print("addition :",addition)
+subtraction=(number_1-number_2)
+print("subraction :",subtraction)
+multiplication=(number_1*number_2)
+print("multiplication:",multiplication)
+division=(number_1/ number_2)
+print("division:",division)
+#Task2
+First_name=input("enter your first name:")
+Last_name=input("enter your last name:")
+print(f"\nHello, {First_name} {Last_name}! Welocme to python programme.")
+
+
